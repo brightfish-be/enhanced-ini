7174446645f30d9a69c535cdcf9c2ae69bcc5497
Brightfish Cinemapub <tech@cinemapub.be>
2019-12-25 22:01:17 +0100

    list sections

--- src/EnhancedIni.php
+++ src/EnhancedIni.php
@@ -4,36 +4,47 @@
 namespace Brightfish\EnhancedIni;
 
 use ErrorException;
-use function PHPUnit\Framework\throwException;
+use function PHPUnit\Framework\throwException ;
 
+/**
+ * Class EnhancedIni
+ * @package Brightfish\EnhancedIni
+ */
 class EnhancedIni
 {
     protected $data=[];
     protected $var_prefix="{";
     protected $var_postfix="}";
-    protected $default_chapter="default";
+    protected $default_section="default";
     protected $ini_loaded=false;
 
     /**
      * @param $chapter_name
      * @return $this
      */
-    public function set_default_chapter($chapter_name){
-        $this->default_chapter=$chapter_name;
+    public function set_default_section($section){
+        $this->default_section=$section;
         return $this;
     }
+    public function set_default_chapter($section){
+        return $this->set_default_section($section);
+    }
+
 
     /**
      * @param $substitute_characters
      * @return $this
      * @throws ErrorException
      */
-    public function set_delimiters($substitute_characters){
-        if(!$substitute_characters){
+    public function set_delimiters($delimiters){
+        if(!$delimiters){
             throw New ErrorException("Delimiters cannot be empty");
         }
-        $this->var_prefix=substr($substitute_characters,0,-1);
-        $this->var_postfix=substr($substitute_characters,-1,1);
+        // if $delimiters == "#"    =>  parameters are like #name#
+        // if $delimiters == "{}"   =>  parameters are like {name}
+        // if $delimiters == "[i]"  =>  parameters are like [name]
+        $this->var_prefix=substr($delimiters,0,-1);
+        $this->var_postfix=substr($delimiters,-1,1);
         return $this;
     }
 
@@ -44,7 +55,12 @@ class EnhancedIni
      * @return $this
      * @throws ErrorException
      */
+
     public function load_ini($filename,$typed_values=true,$stop_on_syntax_error=false){
+        return $this->load_from_file($filename,$typed_values,$stop_on_syntax_error);
+    }
+
+    public function load_from_file($filename,$typed_values=true,$stop_on_syntax_error=false){
         if(!file_exists($filename)){
             throw New ErrorException("File [$filename] does not exist");
         }
@@ -62,44 +78,46 @@ class EnhancedIni
         return $this;
     }
 
-    protected function has_syntax_errors($filename){
-        $raw=file_get_contents($filename);
-        $lines=explode("\n",$raw);
-        $sections=[];
-        $warnings=[];
-        foreach($lines as $line){
-            if($this->preg_extract("^\[(%b)\]",$line)){
-                // is section header
-                $section_name=$this->preg_extract("^\[(%b)\]",$line);
-                if(isset($sections[$section_name])){
-                    $warnings[]=sprintf("[%s]: duplicate section definition",$section_name);
-                } else {
-                    $sections[$section_name]=$section_name;
-                }
-            }
-            if($this->preg_extract("^[\s\t]+\[(%b)\]",$line)){
-                // is section header
-                $section_name=$this->preg_extract("\[(%b)\]",$line);
-                $warnings[]=sprintf("[%s]: spaces before section definition",$section_name);
+    public function load_from_string($input,$typed_values=true,$stop_on_syntax_error=false){
+        if($stop_on_syntax_error){
+            if($this->has_syntax_errors($input,true)){
+                return $this;
             }
         }
-        //print_r($warnings);
-        return $warnings;
+        if($typed_values){
+            $this->data=parse_ini_string($input,true,INI_SCANNER_TYPED);
+        } else {
+            $this->data=parse_ini_string($input,true);
+        }
+        $this->ini_loaded=true;
+        return $this;
     }
+
     /**
      * @param $key
      * @param bool $chapter
      * @return bool|mixed
+     * alias for get - to look more like a resource controller
+     */
+    public function show($key,$chapter=false)
+    {
+        return $this->get($key,$chapter);
+    }
+
+    /**
+     * @param $key
+     * @param bool $chapter
+     * @return bool|string|array
      */
     public function get($key,$chapter=false){
         if(!$this->ini_loaded)  return false;
 
-        if(!$chapter)   $chapter=$this->default_chapter;
+        if(!$chapter)   $chapter=$this->default_section;
         if(isset($this->data[$chapter][$key])){
             return $this->resolve_value($this->data[$chapter][$key],$chapter);
         }
-        if($chapter <> $this->default_chapter AND isset($this->data[$this->default_chapter][$key])){
-            return $this->resolve_value($this->data[$this->default_chapter][$key],$chapter);
+        if($chapter <> $this->default_section AND isset($this->data[$this->default_section][$key])){
+            return $this->resolve_value($this->data[$this->default_section][$key],$chapter);
         }
         return false;
     }
@@ -107,39 +125,62 @@ class EnhancedIni
     /**
      * @param string $chapter
      * @return array
+     * alias for get_all - to look more like a resource controller
      */
-    public function get_all($chapter="")
+    public function index($chapter=""){
+        return $this->get_all($chapter);
+    }
+
+    /**
+     * @param string $section
+     * @return array
+     */
+    public function get_all($section="")
     {
         if(!$this->ini_loaded)  return [];
-        if(!$chapter){
-            // no chapter specified - get all chapters - recursive
+        if(!$section){
+            // no section specified - get all sections and recurse
             $resolved=[];
-            foreach(array_keys($this->data) as $chapter){
-                if($chapter AND $chapter <> $this->default_chapter){
-                    $resolved[$chapter]=$this->get_all($chapter);
-                }
+            $sections=$this->get_sections();
+
+            foreach($sections as $section){
+                $resolved[$section]=$this->get_all($section);
             }
             return $resolved;
         } else {
-            if(!isset($this->data[$chapter])){
-                // this chapter doesn't exist
+            if(!isset($this->data[$section])){
+                // this section doesn't exist
                 return [];
             }
             $resolved=[];
-            if(is_array($this->data[$chapter])){
-                if(isset($this->data[$this->default_chapter])){
-                    $keys=array_merge(array_keys($this->data[$this->default_chapter]),array_keys($this->data[$chapter]));
+            if(is_array($this->data[$section])){
+                if(isset($this->data[$this->default_section])){
+                    $keys=array_merge(array_keys($this->data[$this->default_section]),array_keys($this->data[$section]));
                 } else {
-                    $keys=array_keys($this->data[$chapter]);
+                    $keys=array_keys($this->data[$section]);
                 }
                 foreach($keys as $key){
-                    $resolved[$key]=$this->get($key,$chapter);
+                    $resolved[$key]=$this->get($key,$section);
                 }
             } else {
-                $resolved=$this->data[$chapter];
+                $resolved=$this->data[$section];
             }
             return $resolved;
         }
+    }
+
+    /**
+     * @return array
+     */
+    public function get_sections(){
+        $sections=[];
+        foreach(array_keys($this->data) as $section){
+            if($section <> $this->default_section){
+                $sections[$section]=$section;
+            }
+        }
+        ksort($sections);
+        return $sections;
     }
 
     // ----------------------- PROTECTED STUFF
@@ -181,29 +222,72 @@ class EnhancedIni
     }
 
     /**
+     * @param $input
+     * @param bool $is_string
+     * @return array
+     */
+    protected function has_syntax_errors($input,$is_string=false){
+        if(!$is_string){
+            // $input is filename
+            $raw=file_get_contents($input);
+        } else {
+            // $input is ini string
+            $raw=$input;
+        }
+        $lines=explode("\n",$raw);
+        $sections=[];
+        $warnings=[];
+        foreach($lines as $line){
+            if($this->preg_extract("^\[(%b)\]",$line)){
+                // is section header
+                $section_name=$this->preg_extract("^\[(%b)\]",$line);
+                if(isset($sections[$section_name])){
+                    $warnings[]=sprintf("[%s]: duplicate section definition",$section_name);
+                } else {
+                    $sections[$section_name]=$section_name;
+                }
+            }
+            if($this->preg_extract("^[\s\t]+\[(%b)\]",$line)){
+                // is section header
+                $section_name=$this->preg_extract("\[(%b)\]",$line);
+                $warnings[]=sprintf("[%s]: spaces before section definition",$section_name);
+            }
+        }
+        //print_r($warnings);
+        return $warnings;
+    }
+
+
+    /**
      * @param $pattern
      * @param $subject
      * @param bool $enhanced
      * @return mixed|string
      */
-    public function preg_extract($pattern,$subject,$enhanced=true){
+    protected function preg_extract($pattern,$subject,$enhanced=true){
         $matches=[];
         if($enhanced){
-            //print_r("preg before = '$pattern'\n");
             if(substr($pattern,0,1)<>substr($pattern,-1,1)){
-                $pattern="|${pattern}|";
-                $pattern=str_replace("%t","[^<>]*",$pattern);
-                $pattern=str_replace("%q","[^\"]*",$pattern);
-                $pattern=str_replace("%b","[^\]\[]*",$pattern);
+                // if pattern doesn't start and end with same character (typically /)
+                switch(true){
+                    case !strstr($pattern,"/"): $pattern="/${pattern}/";    break;
+                    case !strstr($pattern,"#"): $pattern="#${pattern}#";    break;
+                    case !strstr($pattern,"|"): $pattern="|${pattern}|";    break;
+                    default:    $pattern="&${pattern}&";
+                }
+                // replace regex shortcuts
+                $pattern=str_replace("%t","[^<>]*",$pattern);   // everything inside a tag: <(%t)>
+                $pattern=str_replace("%q","[^\"]*",$pattern);   // everything inside double quotes: "(%q)"
+                $pattern=str_replace("%s","[^']*",$pattern);    // everything inside single quotes: '(%s)'
+                $pattern=str_replace("%b","[^\]\[]*",$pattern); // everything inside brackets: \[(%b)\]
+                $pattern=str_replace("%l","[^\n]*",$pattern);   // everything on a line: (%l)\n
             }
-            //print_r("preg after = '$pattern'\n");
         }
         preg_match($pattern,$subject,$matches);
         if($matches){
             return $matches[1];
         }
         return "";
-
     }
 
 }
--- tests/ReadIniTest.php
+++ tests/ReadIniTest.php
@@ -27,24 +27,43 @@ class ReadIniTest extends TestCase
         $this->assertEmpty($ei->get_all());
     }
 
-    public function test_load_normal(){
+    public function test_load_from_file(){
         $ei=New EnhancedIni();
         $ei->load_ini("tests/ini/example.ini");
         $data=$ei->get_all();
         $this->assertArrayHasKey("chapter1",$data);
     }
 
+    public function test_load_from_string(){
+        $ei=New EnhancedIni();
+        $ei->load_from_string("[test]\nkey1=val1\nkey2=val2\n[test2]\nkey=test");
+        $data=$ei->get_all();
+        $this->assertArrayHasKey("test2",$data);
+    }
+
     public function test_get_default(){
         $ei=New EnhancedIni();
         $ei->load_ini("tests/ini/complex.ini");
         $data=$ei->get_all();
-        $this->assertArrayHasKey("file",$data["chapter1"]);
+        $this->assertArrayHasKey("file",$data["section1"]);
     }
 
     public function test_substitute_key(){
         $ei=New EnhancedIni();
         $ei->load_ini("tests/ini/complex.ini");
-        $this->assertEquals("/temp/chapter1.txt",$ei->get("key","chapter1"));
+        $this->assertEquals("/temp/chapter1.txt",$ei->get("key","section1"));
+    }
+
+    public function test_list_sections_count(){
+        $ei=New EnhancedIni();
+        $ei->load_ini("tests/ini/complex.ini");
+        $this->assertEquals(6,count($ei->get_sections()));
+    }
+
+    public function test_list_sections_member(){
+        $ei=New EnhancedIni();
+        $ei->load_ini("tests/ini/complex.ini");
+        $this->assertArrayHasKey("section1",$ei->get_sections());
     }
 
 }
